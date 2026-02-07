279faf618466497480bf91e08ba835893db35585
Phu Khanh <khanhphuhoiquy@gmail.com>
2026-02-07 11:08:07 +0700

    add linkk

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,8 +3,8 @@ export const projects = [
     title: "Shiro's Portfolio",
     description: "A personal portfolio website built with React and Tailwind CSS to showcase my projects and skills.",
     tech: ["React", "Node.js","Tailwind CSS", "JavaScript"],
-    github: "https://github.com/AyameIchii",
-    demo: "https://ayameichii.github.io",
+    github: "https://github.com/AyameIchii/Portfilo.git",
+    demo: "https://ayameichii.github.io/Portfilo/",
   },
    {
     title: "Shiro's ToDoList",
@@ -12,5 +12,12 @@ export const projects = [
     tech: ["React","Tailwind CSS", "JavaScript"],
     github: "https://github.com/AyameIchii/Mini_ToDoo.git",
     demo: "https://ayameichii.github.io/Mini_ToDoo/",
+  },
+  {
+    title: "Shiro's ToDoList",
+    description: "Website To_Do_List được xây dựng bởi Redux và TailwindCSS.",
+    tech: ["Redux","Tailwind CSS", "JavaScript"],
+    github: "https://github.com/AyameIchii/Redux-Todoo.git",
+    demo: "https://ayameichii.github.io/Redux-Todoo/",
   },
 ];
